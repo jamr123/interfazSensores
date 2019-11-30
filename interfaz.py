169f4f2b13b5179d24e6bbe8aa4a37b814803cbd
jorge <jorge.meza.rdz@gmail.com>
2019-11-29 19:35:41 -0600

    init

--- interfaz.py
+++ interfaz.py
@@ -1,8 +1,5 @@
 from Tkinter import * 
 
- 
-
-
 class Interfaz:
    
 
@@ -29,6 +26,32 @@ class Interfaz:
         eS3 = Entry(V)
         eS3 .grid(row=2, column=1, padx=5, pady=5)
         eS3 .config(justify="right", state='readonly')
+
+        eSC1 = Entry(V)
+        eSC1 .grid(row=0, column=3, padx=5, pady=5)
+        eSC1 .config(justify="right", state='readonly')
+         
+        eSC2 = Entry(V)
+        eSC2 .grid(row=1, column=3, padx=5, pady=5)
+        eSC2 .config(justify="right", state='readonly')
+         
+        eSC3 = Entry(V)
+        eSC3 .grid(row=2, column=3, padx=5, pady=5)
+        eSC3 .config(justify="right", state='readonly')
+
+        eSD1 = Entry(V)
+        eSD1 .grid(row=0, column=5, padx=5, pady=5)
+        eSD1 .config(justify="right", state='readonly')
+         
+        eSD2 = Entry(V)
+        eSD2 .grid(row=1, column=5, padx=5, pady=5)
+        eSD2 .config(justify="right", state='readonly')
+         
+        eSD3 = Entry(V)
+        eSD3 .grid(row=2, column=5, padx=5, pady=5)
+        eSD3 .config(justify="right", state='readonly')
+
+        
         
         
     def labels(self,V):
@@ -44,10 +67,20 @@ class Interfaz:
     def botones(self,V):
         b1=Button(V, text="Conectar S1")
         b1.grid(row=0, column=2, padx=5, pady=5)
+
         b2=Button(V, text="Conectar S2")
         b2.grid(row=1, column=2, padx=5, pady=5)
+
         b3=Button(V, text="Conectar S3")
         b3.grid(row=2, column=2, padx=5, pady=5)
 
-    def counters(self,V):
-        print("")
+        b1=Button(V, text="Desconectar S1")
+        b1.grid(row=0, column=4, padx=5, pady=5)
+
+        b2=Button(V, text="Desconectar S2")
+        b2.grid(row=1, column=4, padx=5, pady=5)
+
+        b3=Button(V, text="Desconectar S3")
+        b3.grid(row=2, column=4, padx=5, pady=5)
+
+    
